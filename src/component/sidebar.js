4db73a7962d77534a1ca730307ebeec47ed9034b
gidox1 <gideonodiase1@gmail.com>
2023-05-22 23:57:30 +0100

    Sidebar setup

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from '../assets/logo_eatly1.svg'
 import logout from '../assets/exit.svg';
 import home from '../assets/home.svg';
@@ -7,22 +7,24 @@ import cart from '../assets/cart.svg';
 import profile from '../assets/profile.svg';
 
 const Sidebar = (props) => {
+  const [activeIcon, setActiveIcon] = useState('home');
+
   return (
       <div className="sidebar_wrapper">
         <div className="sidebar_logo">
           <img className="sidebar_logo_image" alt="eatlyLogo" src={logo} />
         </div>
         <div className="sidebar_icons">
-          <div className="sidebar_logo_circle_active">
+          <div className={activeIcon === "home" ? "sidebar_logo_circle_active" : "sidebar_logo_circle"} onClick={() => setActiveIcon('home')}>
             <img className="sidebar_logo_image" alt="eatlyLogo" src={home} />
           </div>
-          <div className="sidebar_logo_circle">
+          <div className={activeIcon === "notification" ? "sidebar_logo_circle_active" : "sidebar_logo_circle"} onClick={() => setActiveIcon('notification')}>
             <img className="sidebar_logo_image" alt="eatlyLogo" src={notification} />
           </div>
-          <div className="sidebar_logo_circle">
+          <div className={activeIcon === "profile" ? "sidebar_logo_circle_active" : "sidebar_logo_circle"} onClick={() => setActiveIcon('profile')}>
             <img className="sidebar_logo_image" alt="eatlyLogo" src={profile} />
           </div>
-          <div className="sidebar_logo_circle">
+          <div className={activeIcon === "cart" ? "sidebar_logo_circle_active" : "sidebar_logo_circle"} onClick={() => setActiveIcon('cart')}>
             <img className="sidebar_logo_image" alt="eatlyLogo" src={cart} />
           </div>
         </div>
