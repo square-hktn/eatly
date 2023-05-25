54afbf07a180c9704dd7aafaa95611ffb184cdc8
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-05-25 18:14:11 -0500

    address modal

--- src/pages/dashboard/dashboard.page.js
+++ src/pages/dashboard/dashboard.page.js
@@ -2,12 +2,12 @@ import React from "react";
 import Sidebar from "../../component/sidebar";
 
 import ConfirmationModal from "../../component/ConfirmationModal";
-import AddressModal
+import AddressModal from '../../component/AddressModal'
 const Dashboard = (props) => {
   return (
       <div className="dashboard_main">
         
-        <div class="ml-5"><ConfirmationModal /></div>
+        <div class="ml-5"><AddressModal /></div>
       </div>
   );
 };
